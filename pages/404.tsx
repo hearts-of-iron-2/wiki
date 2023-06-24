0bfe95f33e77825540f33b87d9c0ef895ceec3b1
Ivan Dimitrov <ivan@ivandimitrov.co.uk>
2023-06-25 00:22:26 +0300

    not found page back to index

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import { getPostSlugs } from "../lib/api";
+import PostSearch from "../components/post-search";
+
+type Props = {
+  articles: string[];
+};
+
+const NotFound = ({ articles }: Props) => {
+  return <PostSearch articles={articles} />;
+};
+
+export async function getStaticProps() {
+  const allSlugs = getPostSlugs().map((s) => s.replace(/\.md$/, ""));
+  return {
+    props: {
+      articles: allSlugs,
+    },
+  };
+}
+
+export default NotFound;
