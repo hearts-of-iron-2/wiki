30360202d54807e950bcaaae8b7ea3a45566333a
Ivan Dimitrov <ivan@ivandimitrov.co.uk>
2023-06-25 18:07:30 +0300

    adding iframe logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import "../styles/index.css";
+import "../styles/iFrame.css";
+
+function inIframe() {
+  try {
+    return window.sessionStorage.getItem("inside-iframe") === "true" ||
+      window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [classes, setClasses] = useState("");
+
+  useEffect(() => {
+    if (inIframe()) {
+      let c = "inside-iframe";
+      window.sessionStorage.setItem("inside-iframe", "true");
+      setClasses(c);
+    }
+  }, []);
+
+  return (
+    <div className={classes}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
