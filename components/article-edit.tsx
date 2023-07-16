b2279d996d2d799d4c3f4e2c3bc66e6c1c99158d
Ivan Dimitrov <ivan@idimitrov.dev>
2023-07-16 19:18:56 +0300

    testing husky

---
+++ components/article-edit.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  markdown: string;
+};
+
+const ArticleBody = ({ markdown }: Props) => {
+  return <div>hehe</div>;
+};
+
+export default ArticleBody;
--- interfaces/article.ts
+++ interfaces/article.ts
@@ -2,6 +2,7 @@ type ArticleType = {
   slug: string;
   title: string;
   content: string;
+  markdown: string;
 };
 
 export default ArticleType;
