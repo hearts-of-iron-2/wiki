5a567aa56fadfa5f0bd213a6cbc41372331b28af
Ivan Dimitrov <ivan@idimitrov.dev>
2023-07-22 08:43:21 +0300

    making the svg icons visible on all browsers

--- components/icons/general.tsx
+++ components/icons/general.tsx
@@ -1,13 +1,18 @@
+import svgStyles from "./svg-styles.module.css";
+
 export const EditIcon = () => {
   return (
     <svg
-      className="w-6 h-6 text-gray-800 dark:text-white"
+      className={svgStyles["svg"]}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
       viewBox="0 0 20 18"
     >
-      <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z" />
+      <path
+        fill="currentColor"
+        d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"
+      />
       <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z" />
     </svg>
   );
@@ -16,16 +21,16 @@ export const EditIcon = () => {
 export const GitHubIcon = () => {
   return (
     <svg
-      className="w-6 h-6 text-gray-800 dark:text-white"
+      className={svgStyles["svg"]}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
       viewBox="0 0 20 20"
     >
       <path
-        fillRule="evenodd"
+        fill-rule="evenodd"
         d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-        clipRule="evenodd"
+        clip-rule="evenodd"
       />
     </svg>
   );
@@ -34,7 +39,7 @@ export const GitHubIcon = () => {
 export const DiscordIcon = () => {
   return (
     <svg
-      className="w-6 h-6 text-gray-800 dark:text-white"
+      className={svgStyles["svg"]}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -48,17 +53,18 @@ export const DiscordIcon = () => {
 export const SearchIcon = () => {
   return (
     <svg
+      className={svgStyles["svg"]}
+      aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
+      viewBox="0 0 20 20"
     >
       <path
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        strokeWidth="2"
+        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
       />
     </svg>
   );
