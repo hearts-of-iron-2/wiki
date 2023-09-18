e777089384ab6754f987dace6d9eda73e66af5d6
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-18 18:55:36 +0300

    updating flakes and packages

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
-  description = "Run 'nix develop' to have a dev shell that has everything this project needs";
+  description = ''
+    Run 'nix develop' to have a dev shell that has everything this project needs.
+    Run `nix build` to build this project.
+    Run `nix run` to run this project.
+  '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs";
     systems.url = "github:nix-systems/x86_64-linux";
     flake-utils = {
       url = "github:numtide/flake-utils";
@@ -14,16 +18,50 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pname = "wiki.hoi2bunker.com";
+        version = "1.0.0";
+        buildInputs = with pkgs; [
+          pandoc
+          nodejs_20
+          nodePackages_latest.pnpm
+        ];
+        nativeBuildInputs = buildInputs;
+        npmDepsHash = "sha256-Etk5w+kCjXPzu7b01/ULlFkoUo1Pk+C23MmA/UyCfkY=";
+        projectRoot = ./.;
+        tmuxConfig = ''
+          tmux new-session -s my_session -d
+          tmux new-window -t my_session:2
+          tmux split-window -h -t my_session:2
+          tmux send-keys -t my_session:1.0 'vi' C-m
+          tmux send-keys -t my_session:2.0 'pnpm dev' C-m
+          tmux attach-session -t my_session
+        '';
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            haskellPackages.pandoc
-            nodejs_20
-            nodePackages_latest.pnpm
-          ];
+          inherit buildInputs;
+          shellHook = ''
+            #!/usr/bin/env bash
+            ${tmuxConfig}
+          '';
+        };
+        packages.default = pkgs.buildNpmPackage {
+          inherit version buildInputs npmDepsHash nativeBuildInputs;
+          pname = "wiki.hoi2bunker.com";
+          src = ./.;
+          postInstall = ''
+            mkdir -p $out/bin
+            exe="$out/bin/${pname}"
+            lib="$out/lib/node_modules/${pname}"
+            cp -r ./.next $lib
+            touch $exe
+            chmod +x $exe
+            echo "
+                #!/usr/bin/env bash
+                cd $lib
+                ${pkgs.nodePackages_latest.pnpm}/bin/pnpm run start" > $exe
+          '';
         };
       });
 }
-
 
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,54 +1,64 @@
-import { createClient } from "@supabase/supabase-js";
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import { runInBrowser } from "./clientside";
 
 const projectUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const storage = {
-  getItem: (key: string) => {
-    return new Promise<string>((resolve, reject) => {
-      try {
-        runInBrowser(() => {
-          const item = window.localStorage.getItem(key);
-          resolve(item);
+    getItem: (key: string) => {
+        return new Promise<string>((resolve, reject) => {
+            try {
+                runInBrowser(() => {
+                    const item = window.localStorage.getItem(key);
+                    resolve(item);
+                });
+                resolve(null);
+            } catch (error) {
+                reject(error);
+            }
         });
-        resolve(null);
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
-  setItem: (key: string, value: string) => {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        runInBrowser(() => {
-          window.localStorage.setItem(key, value);
+    },
+    setItem: (key: string, value: string) => {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                runInBrowser(() => {
+                    window.localStorage.setItem(key, value);
+                });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
         });
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
-  removeItem: (key: string) => {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        runInBrowser(() => {
-          window.localStorage.removeItem(key);
+    },
+    removeItem: (key: string) => {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                runInBrowser(() => {
+                    window.localStorage.removeItem(key);
+                });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
         });
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
+    },
 };
 
-export const supabase = createClient(projectUrl, anonKey, {
-  auth: {
-    detectSessionInUrl: true,
-    persistSession: true,
-    flowType: "pkce",
-    storage,
-  },
-});
+const client = () => {
+    let client: SupabaseClient<any, "public", any> = undefined
+    try {
+        client = createClient(projectUrl, anonKey, {
+            auth: {
+                detectSessionInUrl: true,
+                persistSession: true,
+                flowType: "pkce",
+                storage,
+            },
+        });
+    } catch {
+        console.error("supabase client failed to init")
+    }
+    return client
+}
+
+export const supabase = client()
