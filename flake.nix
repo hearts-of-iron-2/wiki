7074ef556a0d95992a0851dfd87e3235b5133322
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-15 19:04:41 +0200

    flake cleanup

--- flake.nix
+++ flake.nix
@@ -18,22 +18,15 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pname = "wiki.hoi2bunker.com";
-        version = "1.0.0";
         buildInputs = with pkgs; [
-          pandoc
           nodejs_20
-          nodePackages_latest.pnpm
+          nodePackages_latest.npm-check-updates
+          bun
         ];
-        nativeBuildInputs = buildInputs;
-        npmDepsHash = "sha256-Etk5w+kCjXPzu7b01/ULlFkoUo1Pk+C23MmA/UyCfkY=";
-        projectRoot = ./.;
         tmuxConfig = ''
           tmux new-session -s my_session -d
           tmux new-window -t my_session:2
-          tmux split-window -h -t my_session:2
           tmux send-keys -t my_session:1.0 'vi' C-m
-          tmux send-keys -t my_session:2.0 'pnpm dev' C-m
           tmux attach-session -t my_session
         '';
       in
@@ -41,24 +34,7 @@
         devShell = pkgs.mkShell {
           inherit buildInputs;
           shellHook = ''
-            #!/usr/bin/env bash
             ${tmuxConfig}
-          '';
-        };
-        packages.default = pkgs.buildNpmPackage {
-          inherit pname version buildInputs npmDepsHash nativeBuildInputs;
-          src = ./.;
-          postInstall = ''
-            mkdir -p $out/bin
-            exe="$out/bin/${pname}"
-            lib="$out/lib/node_modules/${pname}"
-            cp -r ./.next $lib
-            touch $exe
-            chmod +x $exe
-            echo "
-                #!/usr/bin/env bash
-                cd $lib
-                ${pkgs.nodePackages_latest.pnpm}/bin/pnpm run start" > $exe
           '';
         };
       });
